de1ea4c16684c96a648790e6fc39e37c658b2108
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-02-10 13:03:18 +0400

    ugly stuff

--- udemi-tuts/src/App.vue
+++ udemi-tuts/src/App.vue
@@ -1,19 +1,19 @@
 <template>
-    <div>{{ name }}</div>
+    <div>
+        <app-header></app-header>
+        <div>
+            content
+        </div>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
+    import Footer from './components/header_footer/Footer.vue';
     export default {
-        data(){
-            return {
-                name:'Francis'
-            }
+        components:{
+            'app-footer': Footer
         }
+       
     }
 </script>
-
-<style>
-    div {
-        color:red
-    }
-</style>
--- udemi-tuts/src/main.js
+++ udemi-tuts/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import Header from './components/header_footer/Header';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.component('app-header',Header);
+app.mount('#app');
