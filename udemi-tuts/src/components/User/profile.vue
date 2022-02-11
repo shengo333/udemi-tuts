6ad93eb438b46fa3834834bed4528bb0cc459733
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-02-11 10:13:40 +0400

    props

---
+++ udemi-tuts/src/components/User/profile.vue
@@ -0,0 +1,37 @@
+<template>
+        <div class="user_profile">
+            <h3>User information</h3>
+            <ul>
+                <li>
+                    <span>Name:</span> {{ name }}
+                </li>
+                 <li>
+                    <span>Known as:</span> {{ alsoKnowAs }}
+                </li>
+                <li>
+                    <span>Lastname:</span> {{ lastname }}
+                </li>
+            </ul>
+        </div>
+</template>
+
+<script>
+    export default {
+        props:['alsoKnowAs','lastname'],
+        data(){
+            return {
+                name:'Steve'
+            }
+        }
+    }
+</script>
+
+<style>
+    span {
+        font-weight: 800;
+    }
+    .user_profile {
+        border:1px solid #2196f3;
+        padding:10px
+    }
+</style>
