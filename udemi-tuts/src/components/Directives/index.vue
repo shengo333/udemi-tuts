11b5e635facb5cdc30123313feb68f0ee1759735
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-02-23 09:55:53 +0400

    some updates

--- udemi-tuts/src/components/Directives/index.vue
+++ udemi-tuts/src/components/Directives/index.vue
@@ -1,8 +1,13 @@
 <template>
+
+    <div v-user-lastname="lastname"></div>
     <div v-awesome.red.big="textToRender"></div>
 
     <button @click="this.textToRender = 'Something else'">
         Trigger update
+    </button>
+        <button @click="this.lastname = 'changed last name'">
+        change jones
     </button>
 
 </template>
@@ -11,7 +16,18 @@
 export default {
     data(){
         return{
-            textToRender:'Text from data'
+            textToRender:'Text from data',
+            lastname:'Jones'
+        }
+    },
+    directives:{
+        'user-lastname':{
+            beforeMount(el,binding){
+                el.innerHTML =  binding.value;
+            },
+            beforeUpdate(el,binding){
+                el.innerHTML =  binding.value;
+            }
         }
     }
 }    
