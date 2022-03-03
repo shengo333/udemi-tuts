3fc833feb7f908697e0bbdbe88b040e6bc7fd0d6
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-03 09:41:20 +0400

    forms

---
+++ forms/forms/src/components/contact/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <form>
+    <div class="row">
+      <div class="col-xl-12">
+        <h1>Contact us</h1>
+        <hr />
+
+        <div class="form-group">
+          <label for="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            class="form-control"
+            v-model.lazy="formData.name"
+          />
+        </div>
+ 
+        <div class="form-group">
+          <label for="email">email</label>
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            v-model="formData.email"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="subject">Subject</label>
+          <input
+            type="text"
+            id="subject"
+            class="form-control"
+            v-model="formData.subject"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="message">Message</label>
+          <textarea
+            class="form-control"
+            rows="3"
+            id="message"
+            v-model="formData.message"
+          ></textarea>
+        </div>
+
+
+        <div class="form-group">
+            <h5>Want more spam ? </h5>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="Newsletter" id="newsletter">
+                <label class="form-check-label" for="newsletter">
+                    Newsletter
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="Promotions" id="promotions">
+                <label class="form-check-label" for="newsletter">
+                    Promotions
+                </label>
+            </div>
+         </div>
+
+        <div class="form-group">
+            <h5>What are you ? </h5>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" id="human" value="human" name="origin">
+                <label class="form-check-label" for="human">
+                    Human
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" id="alien" value="alien" name="origin">
+                <label class="form-check-label" for="alien">
+                    Alien
+                </label>
+            </div>
+        </div>
+
+        <div class="form-group">
+          <label for="country">Country</label>
+          <select class="form-control" id="country">
+            <option>
+              Francisland
+            </option>
+          </select>
+        </div>
+
+        <button
+          class="btn btn-primary"
+          @click.prevent="submitForm"
+        >
+          Submit
+        </button>
+
+       
+      </div>
+    </div>
+  </form>
+</template>
+
+
+<script>
+  export default {
+    data(){
+      return {
+        formData:{
+          name:'',
+          email:'',
+          subject:'',
+          message:''
+        }
+      }
+    },
+    methods:{
+      submitForm(){
+        console.log(JSON.stringify(this.formData))
+      }
+    }
+  }
+</script>
