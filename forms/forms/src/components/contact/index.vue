3e507daa284016eb2741ee6f5880332ad8061639
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-10 17:27:39 +0400

    form validation

--- forms/forms/src/components/contact/index.vue
+++ forms/forms/src/components/contact/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <form>
+  <form
+    @submit="checkForm"
+  >
     <div class="row">
       <div class="col-xl-12">
         <h1>Contact us</h1>
@@ -88,20 +90,34 @@
 
         <div class="form-group">
           <label for="country">Country</label>
-          <select class="form-control" id="country" v-model="formData.country">
-            <option   v-for="(country, index) in countries"
-              :key="index+country">
-              {{country}}
+          <select class="form-control" id="country"
+            v-model="formData.country"
+          >
+            <option 
+              v-for="(country, index) in countries"
+              :key="index+country"
+            >
+              {{ country }}
             </option>
           </select>
         </div>
 
         <button
           class="btn btn-primary"
-          @click.prevent="submitForm"
         >
           Submit
         </button>
+
+        <hr/>
+
+        <p v-if="this.errors.length">
+            <b>Ooops, fix the errors:</b>
+            <ul>
+              <li v-for="error in errors" :key="error">
+                {{ error }}
+              </li>
+            </ul>
+        </p>
 
        
       </div>
@@ -114,6 +130,7 @@
   export default {
     data(){
       return {
+        errors:[],
         countries:[
           'EEUU',
           'India',
@@ -133,8 +150,32 @@
       }
     },
     methods:{
+      checkForm(e){
+        e.preventDefault();
+        this.errors = [];
+
+        if(!this.formData.name){
+          this.errors.push('Sorry, the name is required')
+        }
+
+        if(!this.formData.email){
+          this.errors.push('Sorry, the email is required')
+        } else if(!this.validEmail(this.email)){
+          this.errors.push('Sorry, the email is not valid')
+        }
+
+        if(!this.errors.length){
+          this.submitForm()
+        }
+
+      },
       submitForm(){
         console.log(JSON.stringify(this.formData))
+      },
+      validEmail(email){
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return re.test(email);
       }
     }
   }
