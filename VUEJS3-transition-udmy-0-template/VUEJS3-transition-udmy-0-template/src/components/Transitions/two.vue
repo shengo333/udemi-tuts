8ac67f7f37ea845cdf885016496475c237def63c
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-20 11:20:03 +0400

    js animation

--- VUEJS3-transition-udmy-0-template/VUEJS3-transition-udmy-0-template/src/components/Transitions/two.vue
+++ VUEJS3-transition-udmy-0-template/VUEJS3-transition-udmy-0-template/src/components/Transitions/two.vue
@@ -1,88 +1,128 @@
 <template>
-    <div>
-        <button class="btn btn-primary" @click="status=!status">Toggle status</button>
+        <button class="btn btn-primary" @click="interrupt = !interrupt">
+           INTERRUPT
+        </button>
+    
+
+    <hr/>
+    <div v-if="interrupt">
+        <button class="btn btn-primary" @click="status = !status">
+            Toggle status
+        </button>
+
         <transition 
-            mode="out-in" 
-            name="shengo" 
+            mode="out-in"
+            name="custom"
+            enter-active-class="dog"
+            leave-active-class="cat"
             @before-enter="beforeEnter"
             @enter="enter"
-            @after-enter="afterEnter"    
+            @after-enter="afterEnter"
             @before-leave="beforeLeave"
             @leave="leave"
-            @after-leave="afterLeave"    
+            @after-leave="afterLeave"
+            @enter-cancelled="enterCancelled"
+            @leave-cancelled="leaveCancelled"
+            :css="false"
         >
-            <div class="p-3 mb-2 bg-danger text-white" v-if="!status" key="status_off">
-                OFF
-            </div>
-            <div class="p-3 mb-2 bg-success text-white" v-else key="status_on">
-                ON
-            </div>
+            <div
+                class="p-3 mb-2 bg-danger text-white"
+                v-if="!status"
+                key="status_off"
+            >OFF</div>
+            <div
+                class="p-3 mb-2 bg-success text-white"
+                key="status_on"
+                v-else
+            >ON</div>
         </transition>
 
         <hr/>
-        
-        <button class="p-3 mb-2 bg-success text-white" @click="library = !library">
-            Toggle library animation
+
+        <button class="btn btn-primary" @click="library = !library">
+            Toggle library anim
         </button>
-        <transition 
-            name="costume-class-animate-css" 
-            enter-active-class="animate__animated animate__backInDown" 
+        <transition
+            name="custom-class-not-to-confuse-with-other-ones"
+            enter-active-class="animate__animated animate__backInDown"
             leave-active-class="animate__animated animate__backOutLeft"
         >
-            <div class="p-3 mb-2 bg-info text-white" v-if="library">
-                animate css transition
-            </div>
+              <div
+                class="p-3 mb-2 bg-info text-white"
+                v-if="library"
+            >Hello</div>
         </transition>
+
+     
 
     </div>
 </template>
 
 <script>
-export default {
-    data(){
-        return{
-            status: false,
-            library: false,       
-        }   
-    },
-    methods: {
-        beforeEnter(){
-            console.log('before enter')
+    import Velocity from 'velocity-animate';
+
+    export default {
+        data(){
+            return {
+                status: false,
+                library: false,
+                interrupt:true
+            }
         },
-        enter(){
-            console.log('enter')
-        },
-        afterEnter(){
-            console.log('after enter')
-        },
-        beforeLeave(){
-            console.log('before leave')
-        },
-        leave(el){
-            console.log('leave', el)
-        },
-        afterLeave(){
-            console.log('after leave')
-        },
+        methods:{
+            beforeEnter(el){
+                el.style.opacity = 0;
+            },
+            enter(el,done){
+                Velocity(el,{
+                    opacity:1, fontSize:'20px'
+                },{
+                    duration:2000,
+                    complete: done
+                });
+            },
+            afterEnter(){
+                console.log('afterEnter')
+            },
+            beforeLeave(){
+                console.log('beforeLeave')
+            },
+            leave(el,done){
+                Velocity(el,{
+                    rotateZ:'180deg',
+                    opacity:0
+                },{
+                    loop:2,
+                    complete:done
+                })
+            },
+            afterLeave(){
+                console.log('afterLeave')
+            },
+            enterCancelled(){
+                console.log('enterCancelled')
+            },
+            leaveCancelled(){
+                console.log('leaveCancelled')
+            }
+        }
     }
-}
 </script>
 
-
 <style>
-.shengo-enter-from,
-.shengo-leave-to  {
-   opacity: 0;    
-}
+    .custom-enter-from,
+    .custom-leave-to  {
+    opacity: 0;    
+    }
 
-.shengo-enter-active,
-.shengo-leave-active {
-    transition: 0.5s;
-}
+    .dog,
+    .cat {
+        transition:5s;
+    }
 
-.shengo-enter-to,
-.shengo-leave-from {
-    opacity: 1;    
-}
+    .custom-enter-to,
+    .custom-leave-from {
+        opacity: 1;    
+    }
 
 </style>
