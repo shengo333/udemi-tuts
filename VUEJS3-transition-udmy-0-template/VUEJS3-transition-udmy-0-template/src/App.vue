03577e7864905b94ae869636115b69808d4780ea
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-21 12:49:04 +0400

    appear prop

--- VUEJS3-transition-udmy-0-template/VUEJS3-transition-udmy-0-template/src/App.vue
+++ VUEJS3-transition-udmy-0-template/VUEJS3-transition-udmy-0-template/src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <app-header></app-header>
         <div class="container">
-            <compTransitionsTwo></compTransitionsTwo>
+            <compTransitionsthree></compTransitionsthree>
         </div>
     <app-footer></app-footer>
 </template>
 
 <script>
   // import compTransitionsOne from './components/Transitions/one';
-  import compTransitionsTwo from './components/Transitions/two.vue'
+  // import compTransitionsthree from './components/Transitions/three.vue'
+   import compTransitionsthree from './components/Transitions/three.vue'
 
   export default{
     components:{
-      compTransitionsTwo
+      compTransitionsthree
     }
   }
 </script>
---
+++ VUEJS3-transition-udmy-0-template/VUEJS3-transition-udmy-0-template/src/components/Transitions/three.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="form-group">
+        <input v-model="name" class="form-control">
+    </div>
+    <button 
+        class="btn btn-primary" 
+        @click="addOne"
+    >
+        Add one
+    </button>    
+    <hr/>
+    <div>
+        <ul class="pl-0">
+            <transition-group name="fade" appear>
+                <li
+                    class="list-group-item"
+                    v-for="(item,index) in list"
+                    @click="removeItem(index)"
+                    :key="item"
+                >
+                    {{ item }}
+                </li>
+            </transition-group>
+        </ul>
+
+        <transition name="fade" appear>
+            <hello></hello>
+        </transition>
+    </div>
+
+</template>
+
+
+<script>
+
+import hello from './hello.vue'
+
+    export default {
+        components:{
+            hello,
+        },
+        data(){
+            return{
+                list:['Francis','Ron','Martha'],
+                name:''
+            }
+        },
+        methods:{
+            addOne(){
+                if(!this.list.includes(this.name)){
+                    this.list.unshift(this.name)
+                }
+            },
+            removeItem(index){
+                this.list.splice(index,1)
+            }
+        }
+    }
+</script>
+
+
+
+<style>
+
+.fade-enter-from,
+.fade-leave-to  {
+   opacity: 0;    
+}
+
+.fade-enter-active {
+    transition: 1s;
+}
+.fade-leave-active {
+    width:100%;
+    transition: 1s;
+    position: absolute;
+}
+
+.fade-enter-to,
+.fade-leave-from {
+    opacity: 1;    
+}
+
+.fade-move {
+    transition: transform 1s;
+}
+
+
+</style>
