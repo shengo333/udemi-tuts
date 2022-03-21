e219407840ef5e4c9c17d4d790aab826455497b7
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-21 16:58:42 +0400

    get requests

--- requests/requests/src/components/Users.vue
+++ requests/requests/src/components/Users.vue
@@ -1,15 +1,25 @@
 <template>
 <div class="row">
 
-     <div class="col-auto mb-4">
+     <div 
+        class="col-auto mb-4"
+        v-for="user in users"
+        :key="user.id"
+     >
         <div class="card" style="width: 14rem;">
-            <img class="card-img-top" src="https://placebeard.it/300/300"  alt="Card image cap">
+            <img 
+            
+                class="card-img-top" 
+                :src="'https://placebeard.it/300/300?'+ user.id"  
+                alt="Card image cap"
+            
+            >
             <div class="card-body">
                 <div class="card-text">
-                    <strong>Name:</strong> Francis
+                    <strong>Name:</strong> {{user.name}}
                 </div>
                 <div class="card-text">
-                    <strong>Lastname:</strong> Jones
+                    <strong>Lastname:</strong> {{user.lastname}}
                 </div>
             </div>
         </div>
@@ -21,4 +31,34 @@
 
 
 <script>
+    import axios from 'axios';
+
+    export default{
+
+        data(){
+            return{
+                users:[]
+            }
+        },
+
+        methods:{
+            loadUsers(){
+                axios.get('http://localhost:3000/users')
+                .then(response => {
+                    this.users = (response.data)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+            }   
+        },
+
+        mounted(){
+            console.log('load data')
+            this.loadUsers()
+        }
+    }
+
+
+
 </script>
