ed939d56a385318f7c55ab3c9541b3e0fb0865ed
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-22 12:52:34 +0400

    loader and errors

--- requests/requests/src/components/Users.vue
+++ requests/requests/src/components/Users.vue
@@ -1,31 +1,36 @@
 <template>
-<div class="row">
+    <div v-if="loading">
+        <app-Loader></app-Loader>
+    </div>
+    
+    
+    <div class="row" v-else>
 
-     <div 
-        class="col-auto mb-4"
-        v-for="user in users"
-        :key="user.id"
-     >
-        <div class="card" style="width: 14rem;">
-            <img 
-            
-                class="card-img-top" 
-                :src="'https://placebeard.it/300/300?'+ user.id"  
-                alt="Card image cap"
-            
-            >
-            <div class="card-body">
-                <div class="card-text">
-                    <strong>Name:</strong> {{user.name}}
-                </div>
-                <div class="card-text">
-                    <strong>Lastname:</strong> {{user.lastname}}
+        <div 
+            class="col-auto mb-4"
+            v-for="user in users"
+            :key="user.id"
+        >
+            <div class="card" style="width: 14rem;">
+                <img 
+                
+                    class="card-img-top" 
+                    :src="'https://placebeard.it/300/300?'+ user.id"  
+                    alt="Card image cap"
+                
+                >
+                <div class="card-body">
+                    <div class="card-text">
+                        <strong>Name:</strong> {{user.name}}
+                    </div>
+                    <div class="card-text">
+                        <strong>Lastname:</strong> {{user.lastname}}
+                    </div>
                 </div>
             </div>
         </div>
-     </div>
 
-</div>
+    </div>
    
 </template>
 
@@ -37,7 +42,8 @@
 
         data(){
             return{
-                users:[]
+                users:[],
+                loading: true
             }
         },
 
@@ -46,10 +52,15 @@
                 axios.get('http://localhost:3000/users')
                 .then(response => {
                     this.users = (response.data)
+                    this.loading= false
                 })
                 .catch(error => {
-                    console.log(error)
+                    this.$toast.error('something wrong', {
+                        positions: 'bottom',   
+                    });
+                    console.log(error);
                 })
+                
             }   
         },
 
--- requests/requests/src/main.js
+++ requests/requests/src/main.js
@@ -3,9 +3,15 @@ import App from './App.vue'
 
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
+import Loader from './components/Utils/loader.vue';
+import Toaster from '@meforma/vue-toaster'
 
 const app =  createApp(App);
 
 app.component('app-header',Header);
 app.component('app-footer',Footer);
+app.component('app-Loader',Loader);
+
+
+app.use(Toaster)
 app.mount('#app')
