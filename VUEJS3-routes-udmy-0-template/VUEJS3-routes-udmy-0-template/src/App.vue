ddf09deb7da255fe69480e2cf31059ebf91375f8
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-03-23 09:23:01 +0400

    routes setting up

--- VUEJS3-routes-udmy-0-template/VUEJS3-routes-udmy-0-template/src/App.vue
+++ VUEJS3-routes-udmy-0-template/VUEJS3-routes-udmy-0-template/src/App.vue
@@ -1,26 +1,17 @@
 <template>
   <app-header></app-header>
         <div class="container">
-          <!-- <home-component></home-component> -->
-          <!-- <articles-main></articles-main> -->
-          <!-- <article-comp></article-comp> -->
+          <router-view></router-view>
           <contact-component></contact-component>
         </div>
     <app-footer></app-footer>
 </template>
 
 <script>
-  // import HomeComponent from './components/index';
-  // import ArticlesMain from './components/Articles'
-  // import ArticleComp from './components/Articles/article'
-  import ContactComponent from './components/Contact'
+
+
 
   export default{
-    components:{
-      // HomeComponent
-      // ArticlesMain
-      // ArticleComp
-      ContactComponent
-    }
+    
   }
 </script>
--- VUEJS3-routes-udmy-0-template/VUEJS3-routes-udmy-0-template/src/main.js
+++ VUEJS3-routes-udmy-0-template/VUEJS3-routes-udmy-0-template/src/main.js
@@ -1,11 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import Header from './components/header_footer/Header';
 import Footer from './components/header_footer/Footer';
+import Articles from './components/Articles';
+import Contact from './components/Contact'
+import Home from './components'
 
 const app =  createApp(App);
 
+const routes = createRouter({
+    history: createWebHashHistory(),
+    routes:[
+        { path:'/', component: Home },
+        { path:'/articles', component: Articles },
+        { path:'/contact', component: Contact },
+    ]
+})
+
+/// yourdomain.com/artis/kkks/ddks
+
+
 app.component('app-header',Header);
 app.component('app-footer',Footer);
+app.use(routes)
 app.mount('#app')
