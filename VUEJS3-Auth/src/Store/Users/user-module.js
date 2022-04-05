06ff6ecb3c4690693bf500496adbb19fdcc64246
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-04-05 12:33:55 +0400

    sign in

--- VUEJS3-Auth/src/Store/Users/user-module.js
+++ VUEJS3-Auth/src/Store/Users/user-module.js
@@ -1,12 +1,40 @@
+/* eslint-disable */
+import axios from 'axios';
+const FAPI_KEY = 'AIzaSyD0CYl2vUBFd16G8lVyGP09n8Xvzh649w4';
+
 const userModule = {
-    namespaced: true,
+    namespaced:true,
     state(){
-        return{
-            name: 'Francis'
+        return {
+            email:'',
+            token:'',
+            refresh:''
+        }
+    },
+    mutations:{
+        authUser(state,payload){
+            state.email = payload.email;
+            state.token = payload.idToken;
+            state.refresh = payload.refreshToken;
+        }
+    },
+    actions:{
+        setToken(context,payload){
+            localStorage.setItem("token",payload.idToken);
+            localStorage.setItem("refresh",payload.refreshToken);
+        },
+        signup(context,payload){
+            axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${FAPI_KEY}`,{
+                ...payload,
+                returnSecureToken:true
+            })
+            .then( response => {
+                context.commit('authUser',response.data);
+                context.dispatch('setToken',response.data)
+            })
+            .catch( error => console.log(error))
         }
     }
 }
-
-
 
 export default userModule;
--- VUEJS3-Auth/src/components/User/signin.vue
+++ VUEJS3-Auth/src/components/User/signin.vue
@@ -3,10 +3,7 @@
        <div class="content sign">
           
           <form @submit.prevent="onSubmit">
-            <h1>
-                <span v-if="type">sing in</span>
-                <span v-else>sign up</span>
-            </h1>
+            <h1>Sign in</h1>
            
               <div class="input_field">
                   <label>Email</label>
@@ -19,13 +16,13 @@
               </div>
               
               <button>
-                <span v-if="type">sing in</span>
-                <span v-else>sign up</span>
+                    <span v-if="type">Sign in</span>
+                    <span v-else>Sign up</span>
               </button>
 
               <div class="change_type" @click="type =!type">
-                <span v-if="type">I want to sing up</span>
-                <span v-else>I want to sign in</span>
+                    <span v-if="type">I want to sign up</span>
+                    <span v-else>I want to sign in</span>
               </div>
 
           </form>
@@ -50,15 +47,14 @@
             onSubmit () {
                 if(this.type){
                     console.log('sign in')
-                } else{
-                    console.log('sign up')
+                }else{
+                    this.$store.dispatch('user/signup',this.formdata)
+
                 }
-
-
                 console.log(this.formdata)
             }
         },
-        mounted() {
+        mounted(){
             console.log(this.$store.state.user.name)
         }
     }
